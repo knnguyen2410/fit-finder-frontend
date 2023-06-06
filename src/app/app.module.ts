622f1c1c6670e14f3c77e840c32c3e88591a36a3
kimnguyen <knnguyen2410@gmail.com>
2023-06-06 18:10:12 -0500

    create service and include httpclientmodule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { GymsComponent } from './gyms/gyms.component';
 import { OwnersComponent } from './owners/owners.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomePageModule
+    HomePageModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
