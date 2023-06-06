389764a48f144e64e1af736e496bb26631733705
kimnguyen <knnguyen2410@gmail.com>
2023-06-06 09:03:09 -0500

    create home component to house homepage

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    HeroComponent
+    HeroComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
