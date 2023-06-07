54699a579ac3357b13b3ab922473e7d1360997cb
kimnguyen <knnguyen2410@gmail.com>
2023-06-06 19:41:48 -0500

    pass gym api data into featured gyms component

--- src/app/home-page/featured-gyms/featured-gyms.component.ts
+++ src/app/home-page/featured-gyms/featured-gyms.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Gym } from 'src/app/models/gym.model';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-featured-gyms',
   templateUrl: './featured-gyms.component.html',
   styleUrls: ['./featured-gyms.component.css']
 })
-export class FeaturedGymsComponent {}
+export class FeaturedGymsComponent implements OnInit {
+
+  gymList: Gym[] = [];
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(){
+    this.apiService.getAllGyms().subscribe((gyms: any) => {
+      this.gymList = gyms
+      console.log(gyms);
+    });
+  }
+}
