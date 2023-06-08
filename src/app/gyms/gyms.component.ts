c732d8135d91790f71e219cd9be47b7714957e06
kimnguyen <knnguyen2410@gmail.com>
2023-06-08 13:08:38 -0500

    create auth service

--- src/app/gyms/gyms.component.ts
+++ src/app/gyms/gyms.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Gym } from '../models/gym.model';
 import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router';
+import { Owner } from '../models/owner.model';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-gyms',
@@ -8,15 +11,61 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./gyms.component.css']
 })
 export class GymsComponent implements OnInit {
-  
-  gymList: Gym[] = [];
+  ownerList: Owner[] = [];
 
-  constructor(private apiService: ApiService){}
+  gymList: Gym[] = [];
+  gymId: number = 0;
+
+  gym: Gym = {
+    id: 0,
+    name: '',
+    category: '',
+    addressStreet: '',
+    addressCity: '',
+    addressState: '',
+    addressZip: 0,
+    hours: '',
+    phone: '',
+    details: '',
+    image: '',
+    owner: '',
+    equipmentList: [],
+    amenityList: [],
+  }
+
+  constructor(private router: Router, private apiService: ApiService){}
 
   ngOnInit(){
     this.apiService.getAllGyms().subscribe((gyms: any) => {
       this.gymList = gyms
       console.log(gyms);
     });
+
+    if (this.hasJWT() == true) {
+      let loggedInOwner = this.ownerList.find(owner => owner.email === LoginComponent.loginRequest.email)
+      if (loggedInOwner) {
+        this.ownerId = loggedInOwner.id;
+        this.router.navigate(['/owners', this.ownerId]);  
+      }
+    }
+    
   }
+
+  hasJWT(): boolean {
+    let jwt = localStorage.getItem('jwt');
+    return jwt !== null && jwt !== undefined && jwt !== '';
+  }
+
+  onSubmit() {
+    this.apiService.createGym(this.gym).subscribe((response: any) => {
+      console.log(response);
+      console.log(this.gym);
+
+      let createdGym = this.gymList.find(gym => gym.name === this.gym.name);
+      if (createdGym) {
+        this.gymId = createdGym.id;
+        this.router.navigate(['/gyms', this.gymId]);
+      }
+    });
+  }  
 }
